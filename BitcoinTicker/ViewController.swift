6f6e4880456b95b931c4eab4b90ad98ee0c29a24
SpongeIsNotBoB <karachunvladislav@gmail.com>
2018-07-06 01:58:55 +0300

    completed bitcoinTicker

---
+++ BitcoinTicker/ViewController.swift
@@ -0,0 +1,72 @@
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let symbolsArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    
+    var finalURL = ""
+    var currencySelected = ""
+
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        super.viewDidLoad()
+       
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        finalURL = baseURL + currencyArray[row]
+        getData(url: finalURL)
+        currencySelected = symbolsArray[row]
+    }
+    
+    //MARK: - Networking
+    /***************************************************************/
+    func getData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+
+                    print("Sucess! Got the data")
+                    let dataJSON : JSON = JSON(response.result.value!)
+
+                    self.updateData(json: dataJSON)
+
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+
+    }
+
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    func updateData(json : JSON) {
+        if let bitcoinResult = json["ask"].double {
+            self.bitcoinPriceLabel.text = "\(currencySelected)\(bitcoinResult)"
+        } else {
+            self.bitcoinPriceLabel.text = "Cant read"
+        }
+    }
+}
+
